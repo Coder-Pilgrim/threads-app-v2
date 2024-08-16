67a41675a806907be0fae13c1cb2c97806cdb516
Enes İnce <81353748+Coder-Pilgrim@users.noreply.github.com>
2024-08-17 00:56:30 +0200

    fixing small issue

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,7 @@ const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
   "/sign-up(.*)",
   "/api/webhook/clerk",
+  "/api/uploadthing",
 ]);
 
 export default clerkMiddleware((auth, request) => {
